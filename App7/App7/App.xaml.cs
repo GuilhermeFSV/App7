2275a4a8e5e2c8e2cc1f4ea4f4606c02c3094518
Guilherme Fernandes <guilherme_fsv@hotmail.com>
2019-05-21 10:03:02 -0300

    Trabalho

--- App7/App7/App.xaml.cs
+++ App7/App7/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using App7.Modelos;
+
 
 namespace App7
 {
--- App7/App7/MainPage.xaml.cs
+++ App7/App7/MainPage.xaml.cs
@@ -4,14 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using App7.Modelos;
 
 namespace App7
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked1(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new CursoPage());
+        }
+
+        void OnButtonClicked2(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new DisciplinaPage());
+        }
+
+        void OnButtonClicked3(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new AlunoPage());
+        }
+
+        void OnButtonClicked4(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new ProfessorPage());
+        }
+
+        void OnButtonClicked5(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new TurmaPage());
+        }
+
+        void OnButtonClicked6(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new MatriculaPage());
+        }
+
+        void OnButtonClicked7(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new HistoricoPage());
+        }
+
+        void OnButtonClicked8(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new ContatoPage());
+        }
+
+    }
 }
+
